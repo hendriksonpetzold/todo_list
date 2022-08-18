43e28b26b769857129f2993d745fd152eb5315ad
Hendrikson Petzold <hendrikson_14@hotmail.com>
2022-08-18 14:22:16 -0300

    refact: delet unnecessary folders

--- lib/presenter/pages/home/to_do_controller.dart
+++ lib/presenter/pages/home/to_do_controller.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -27,13 +26,5 @@ class ToDoController extends GetxController {
         completed.value = element.completed.toString();
       }
     });
-  }
-
-  void onButtonPressed() {
-    for (var title in listString) {
-      if (title == searchEC.text) {
-        text.value = title;
-      }
-    }
   }
 }
